bb5b96c4ccce42ddc35da402d09fdb651c8edf19
Bradley Barker <Bradley.Barker@thoughtware.co.za>
2023-05-22 13:24:48 +0200

    Revert "Test"
    This reverts commit b22576add70fe1977a32ee31b721b4fea61f57f8.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,23 @@
-import React, { useContext } from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Landing, Template1, Template2, ResumeCreate, FourNotFour, Contributors } from './pages'
-import { DetailsContext } from './contexts/DetailsContext'
-import { PDFViewer } from '@react-pdf/renderer';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const { resume } = useContext(DetailsContext);
-
-
-
   return (
-    <div className="app">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <Landing />
-          </Route>
-          <Route path="/template/1" exact>
-            <PDFViewer>
-              <Template1 resume={resume}/>
-            </PDFViewer>
-          </Route>
-
-          <Route path="/template/2" exact>
-            <PDFViewer>
-              <Template2 resume={resume}/>
-            </PDFViewer>
-          </Route>
-
-          <Route path="/create" exact>
-            <ResumeCreate />
-          </Route>
-
-          <Route path="/contributors" exact>
-            <Contributors />
-          </Route>
-
-          <Route component={FourNotFour} />
-
-        </Switch>
-      </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
